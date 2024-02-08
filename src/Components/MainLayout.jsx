05308a92c668935a10101fd7c8021c053ecfe748
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-02-07 22:27:07 -0800

    side nav add

--- src/Components/MainLayout.jsx
+++ src/Components/MainLayout.jsx
@@ -3,41 +3,127 @@
 import "./MainLayout.css";
 import {
   MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
+  MenuUnfoldOutlined
 } from '@ant-design/icons';
+import { AiOutlineBgColors, AiOutlineDashboard, AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
+import { BiCategory } from "react-icons/bi";
+import { FaBloggerB, FaClipboardList, FaListAlt } from "react-icons/fa";
+import { IoList } from "react-icons/io5";
+import { SiBrandfolder } from "react-icons/si";
 import { Layout, Menu, Button, theme } from 'antd';
 import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
 
 const MainLayout = () => {
     const [collapsed, setCollapsed] = useState(false);
+    const navigate =useNavigate()
     return (
         <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="demo-logo-vertical" />
+        <div className="logo" />
         <Menu
           theme="dark"
           mode="inline"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['']}
+          onClick={({key})=>{
+        if(key== "signout"){
+
+        }
+        else{
+         navigate(key)
+        }
+          }}
           items={[
             {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
+              key: '',
+              icon: <AiOutlineDashboard className="text-lg	"/>,
+              label: 'Dashboard',
             },
             {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
+              key: 'customers',
+              icon: <AiOutlineUser />,
+              label: 'Customers',
             },
             {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
+              key: 'catalog',
+              icon: <AiOutlineShoppingCart />,
+              label: 'Catalog',
+              children:[
+                {
+                    key: 'product',
+                    icon: <AiOutlineShoppingCart />,
+                    label: 'Add Product',
+                  },
+                {
+                    key: 'product list',
+                    icon: <AiOutlineShoppingCart />,
+                    label: 'Product List',
+                  },
+                {
+                    key: 'brand',
+                    icon: <SiBrandfolder />,
+                    label: 'Brand',
+                  },
+                {
+                    key: 'brandlist',
+                    icon: <SiBrandfolder />,
+                    label: 'Brand List',
+                  },
+                {
+                    key: 'category',
+                    icon: <BiCategory />,
+                    label: 'Category',
+                  },
+                {
+                    key: 'categorylist',
+                    icon: <BiCategory />,
+                    label: 'Category List',
+                  },
+                {
+                    key: 'color',
+                    icon: <AiOutlineBgColors />,
+                    label: 'Color',
+                  },
+                {
+                    key: 'colorlist',
+                    icon:<AiOutlineBgColors />,
+                    label: 'Color List',
+                  },
+              ]
             },
+            {
+                key: 'orders',
+                icon: <FaClipboardList />,
+                label: 'Orders',
+              },
+            {
+                key: 'blog',
+                icon: <FaBloggerB />,
+                label: 'Blogs',
+                children:[
+                    {
+                        key: 'addblog',
+                        icon: <FaBloggerB />,
+                        label: 'Add Blog',
+                    },
+                    {
+                        key: 'blog-list',
+                        icon: <FaBloggerB />,
+                        label: 'Blog List',
+                    },
+                    {
+                        key: 'blog-category',
+                        icon: <FaBloggerB />,
+                        label: ' Add Blogs Category',
+                    },
+                    {
+                        key: 'blog-category-list',
+                        icon: <FaBloggerB />,
+                        label: 'Blog Category List',
+                    },
+                ]
+              },
           ]}
         />
       </Sider>
