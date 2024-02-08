da552874ab22b9779da66bd6b388f949bdc1179b
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-02-08 01:09:00 -0800

    recent order table create

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,7 +2,33 @@
 /* eslint-disable no-unused-vars */
 import "./Dashboard.css";
 import { BsArrowDownRight } from "react-icons/bs";
+import { Button, Table } from 'antd';
 import { Column } from '@ant-design/plots';
+
+const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+    },
+  ];
+  const data1 = [];
+  for (let i = 0; i < 46; i++) {
+    data1.push({
+      key: i,
+      name: `Edward King ${i}`,
+      age: 32,
+      address: `London, Park Lane no. ${i}`,
+    });
+  }
+
 const Dashboard = () => {
     const data = [
         {
@@ -85,6 +111,8 @@ const Dashboard = () => {
           },
         },
       };
+
+    
     return (
         <div>
             <h5 className="mb-4">DAshboard</h5>
@@ -120,13 +148,17 @@ const Dashboard = () => {
                     </div>
                 </div>
             </div>
-
+{/* pie chart for product sell details */}
             <div className="mt-4">
                 <h3 className="text-2xl font-bold mb-4">Income Statics</h3>
             <div>
             <Column {...config} />
             </div>
             </div>
+
+            {/* Resent Orders */}
+            <h3 className="text-2xl font-bold mb-4 mt-8">Resent Orders</h3>
+            <div>    <Table columns={columns} dataSource={data1} /></div>
         </div>
     );
 };
