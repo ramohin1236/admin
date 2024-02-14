799773d76d70e0b2a673bc6b0c1b2845a7aa7035
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-02-14 10:24:50 -0800

    order page

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,40 +1,88 @@
+/* eslint-disable react/jsx-key */
 
 import { Table } from 'antd';
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getOrders } from '../../features/auth/authSlice';
+import { Link } from 'react-router-dom';
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
 const columns = [
     {
-      title: 'NO.',
-      dataIndex: 'key',
+      title: "SNo",
+      dataIndex: "key",
     },
     {
-      title: 'Name',
-      dataIndex: 'name',
+      title: "Name",
+      dataIndex: "name",
     },
     {
-      title: 'Product',
-      dataIndex: 'product',
+      title: "Product",
+      dataIndex: "product",
     },
     {
-      title: 'Status',
-      dataIndex: 'status',
+      title: "Amount",
+      dataIndex: "amount",
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+    },
+  
+    {
+      title: "Action",
+      dataIndex: "action",
     },
   ];
-  const data1 = [];
-  for (let i = 0; i < 46; i++) {
-    data1.push({
-      key: i,
-      name: `Edward King ${i}`,
-      product: "i-phone",
-      status: `London, Park Lane no. ${i}`,
-    });
-  }
+  
+  
 
 const Orders = () => {
+    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getOrders());
+  }, [dispatch]);
+  const orderState = useSelector((state) => state.auth.orders);
+//   console.log(orderState.orderby.firstname);
+
+  const data1 = [];
+  for (let i = 0; i < orderState.length; i++) {
+    data1.push({
+      key: i + 1,
+      name: orderState[i].orderby.firstname,
+      product: orderState[i].products.map((i,j)=>{
+        return (
+            <div key={j}>
+               <ul >
+                  <li>{i.product.title}</li>
+               </ul>
+            </div>
+        )
+      }),
+    //   product: (
+    //     <Link to={`/admin/order/${orderState[i].orderby._id}`}>
+    //       View Orders
+    //     </Link>
+    //   ),
+      amount: orderState[i].paymentIntent.amount,
+      date: new Date(orderState[i].createdAt).toLocaleString(),
+      action: (
+        <>
+          <Link to="/" className=" fs-3 text-danger">
+            <BiEdit />
+          </Link>
+          <Link className="ms-3 fs-3 text-danger" to="/">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
+    });
+  }
     return (
         <div>
             <h3 className="text-4xl font-bold mb-4">Orders</h3>
-            <div>
-            <Table columns={columns} dataSource={data1} />
+            <div >
+            <Table  columns={columns} dataSource={data1} />
             </div>
         </div>
     );
--- src/features/auth/authService.jsx
+++ src/features/auth/authService.jsx
@@ -1,19 +1,47 @@
-import axios from 'axios';
-import { base_url } from '../../utils/base_url';
+import axios from "axios";
+import { base_url } from "../../utils/base_url";
 
 
+const getTokenFromLocalStorage= localStorage.getItem('user') ?JSON.parse(localStorage.getItem('user')) :null;
 
-const login = async(userData)=>{
-    const response = await axios.post(`${base_url}user/admin-login`,userData)
-   if(response.data){
-    localStorage.setItem("user", JSON.stringify(response.data))
-
-   }
-    return response.data;
+const config ={
+    headers:{
+        Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
+        Accept: "application/json"
+    }
 }
 
-const authService ={
-    login
-}
+
+const login = async (user) => {
+  const response = await axios.post(`${base_url}user/admin-login`, user);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+// const getOrders = async () => {
+//   const response = await axios.get(`${base_url}user/get-orders`,config); 
+
+//   return response.data;
+// };
+const getOrders = async () => {
+  const response = await axios.get(`${base_url}user/get-all-orders`,config); 
+
+  return response.data;
+};
+const getOrder = async (id) => {
+  const response = await axios.post(
+    `${base_url}user/getorderbyuser/${id}`,
+    ""
+  );
+
+  return response.data;
+};
+
+const authService = {
+  login,
+  getOrders,
+  getOrder,
+};
 
 export default authService;
